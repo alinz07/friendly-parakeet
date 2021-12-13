cf8c4379f387bf61685d9ac07a545de4fc784342
Tony Linz <tonyllinz@gmail.com>
2021-12-12 21:04:54 -0600

    finish and test all 4 criteria, works when pwlength and criteria selection initially invalid

--- Develop/script.js
+++ Develop/script.js
@@ -1,66 +1,93 @@
 // Assignment code here
 
 var generatePassword = function() {
-
   var password="";
   var options=[];
   var lowerCase=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
   var upperCase = lowerCase.map(function (a) {
     return a.toUpperCase();
   });
-  var specCharSet=[];
-  var nummies=[];
+  var specCharSet=['/','[','!','@','#','$','%','^','&','*','(',')','_','+','\\','-','=','{','}',';',':','"','|',',','.','<','>','?','+',']'];
+  var nummies=[0,1,2,3,4,5,6,7,8,9];
+  var pwLength = 0;
   
   //ask what length you want the pw to be 8-128 and window alert selection or invaid input
-  pwLength = window.prompt("between integers 8-128 inclusive, how many characters long would you like your password to be?");
+  while (!(pwLength >= 8 && pwLength <=128)) {
+    pwLength = window.prompt("between integers 8-128 inclusive, how many characters long would you like your password to be?");
+    if (!(pwLength >= 8 && pwLength <=128)) {
+      window.alert("Invalid input");
+    }
+    else {
+      continue
+    }
+  }
 
   var criteriaTypes = function() {
-    var criteriaWord=""
-    //window alert that they must choose one of the following 4 character types to include
-    window.alert("You must choose one of the following four character types to generate a password.");
+    while (options.length === 0) {
+      var criteriaWord=""
+      //window alert that they must choose one of the following 4 character types to include
+      window.alert("You must choose one of the following four character types to generate a password.");
   
-    //ask if they want to include lowercase letters and window alert
-    if (window.prompt("Enter '1' to include lowercase letters")=== '1') {
-      window.alert("Thank you, lowercase letters will be included.");
-      //add all lowercase letters to the array 'options' so that we can pull the rest of the characters to fit the pwlength
-      options+=lowerCase;
-      //add at least one of the lowercase letters to the criteriaWord to be add to password
-      criteriaWord += lowerCase[Math.floor(Math.random() * lowerCase.length)];
-    }
-    else {
-      window.alert("Lowercase letters will not be included.");
-    }
+      //ask if they want to include lowercase letters and window alert
+      if (window.prompt("Enter '1' to include lowercase letters, or leave blank and select okay")=== '1') {
+        window.alert("Thank you, lowercase letters will be included.");
+        //add all lowercase letters to the array 'options' so that we can pull the rest of the characters to fit the pwlength
+        options+=lowerCase;
+        //add at least one of the lowercase letters to the criteriaWord to be add to password
+        criteriaWord += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+      }
+      else {
+        window.alert("Lowercase letters will not be included.");
+      }
+
+      //ask if they want to include lowercase letters and window alert
+      if (window.prompt("Enter '2' to include uppercase letters or leave blank and select okay")=== '2') {
+        window.alert("Thank you, uppercase letters will be included.");
+        //add all lowercase letters to the array 'options' so that we can pull the rest of the characters to fit the pwlength
+        options+=upperCase;
+        //add at least one of the lowercase letters to the criteriaWord to be add to password
+        criteriaWord += upperCase[Math.floor(Math.random() * upperCase.length)];
+      }
+      else {
+        window.alert("Uppercase letters will not be included.");
+      }
   
+      //ask if they want to include numbers and window alert
+      if (window.prompt("Enter '3' to include numbers or leave blank and select okay")=== '3') {
+        window.alert("Thank you, numbers will be included.");
+        //add all lowercase letters to the array 'options' so that we can pull the rest of the characters to fit the pwlength
+        options+=nummies;
+        //add at least one of the lowercase letters to the criteriaWord to be add to password
+        criteriaWord += nummies[Math.floor(Math.random() * nummies.length)];
+      }
+      else {
+        window.alert("Numbers will not be included.");
+      }
   
-    //ask if they want to include uppercase letters and window alert
-  
-    //ask if they want to include numbers and window alert
-  
-    //ask if they want to include special characters and window alert
-  
-    //check they chose one of the four options or run again
-  
-    if (options.length === 0) {
-      window.alert("You must select one of the four character types. Thank you for understanding.");
-      criteriaTypes();
-    }
-    else {
+      //ask if they want to include special characters and window alert
+      if (window.prompt("Enter '4' to include special characters or leave blank and select okay")=== '4') {
+        window.alert("Thank you, special characters will be included.");
+        //add all special characters to the array 'options' so that we can pull the rest of the characters to fit the pwlength
+        options+=specCharSet;
+        //add at least one of the lowercase letters to the criteriaWord to be add to password
+        criteriaWord += specCharSet[Math.floor(Math.random() * specCharSet.length)];
+      }
+      else {
+        window.alert("Special characters will not be included.");
+      }
+
+      } //end of while loop
+
       window.alert("Password criteria successfully set, thank you.");
       return criteriaWord;
-    }
-  }
 
-  if (!(pwLength >= 8 && pwLength <=128)) {
-    window.alert("That is not a valid length, please try again.");
-    generatePassword();
-  }
-  
-  else {
-    password+=criteriaTypes();
-    //more code to add more characters from options to criteria word to reach the desired pw length
-    return password;
-  }
-}
+    } //end of criteriaTypes() function
+
+  password+=criteriaTypes();
+  //start here to finish up once commit changes
+  //more code to add more characters from options to criteria word to reach the desired pw length
+  return password;
+} //end of generatePassword() function 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
